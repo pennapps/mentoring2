cd8ac33b3c20bf4df03d05b86ecc200af1ef23b1
roybse3113 <79131282+roybse3113@users.noreply.github.com>
2022-03-25 15:52:35 -0400

    fixed images for deploy

--- src/guides/gitGuide.js
+++ src/guides/gitGuide.js
@@ -4,7 +4,7 @@ import GuidePage from '../GuidePage';
 const gitGuide = () => {
   return (
     <div className="guide-page">
-      <GuidePage guide="Git & Github" img="https://github.com/pennapps/mentoring2/raw/main/platy/platy_git.png" />
+      <GuidePage guide="Git & Github" img="https://github.com/pennapps/mentoring2/raw/main/public/platy/platy_git.png" />
     </div>
   );
 };
--- src/guides/nodeGuide.js
+++ src/guides/nodeGuide.js
@@ -4,7 +4,7 @@ import GuidePage from '../GuidePage';
 const devtoolsGuide = () => {
   return (
     <div className="guide-page">
-      <GuidePage guide="Node js" img="https://github.com/pennapps/mentoring2/raw/main/platy/platy_node.png" />
+      <GuidePage guide="Node js" img="https://github.com/pennapps/mentoring2/raw/main/public/platy/platy_node.png" />
     </div>
   );
 };
