c13e559e7fa48751aff9725b109ded4eeabaefd6
Yuying Fan <yuyingfanus@gmail.com>
2022-10-21 22:16:38 -0400

    added back icon for cards

--- src/components/GuidePage.js
+++ src/components/GuidePage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import {Link} from "gatsby";
 import '../styles/GuidePage.css'
+import backIcon from '../images/icons/icon_back.svg'
 
 const GuidePage = ({pageContext}) => {
   // const img = <img src={props.img} className="GuidePage-image" alt="platy-bg" />;
@@ -9,7 +10,14 @@ const GuidePage = ({pageContext}) => {
   return (
     <div>
       <nav className="guide-nav">
-        <h3><Link to="/" style={{ textDecoration: 'none' }}>PennApps Mentoring</Link></h3>
+        <h3>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            <div className="row">
+              <img src={backIcon} alt="Back Icon" width={28} />
+              <p>Back to Home</p>
+            </div>
+          </Link>
+        </h3>
       </nav>
       <div className="GuidePage-container">
         <div className="guide-info">
